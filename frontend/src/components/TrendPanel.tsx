b49606be3fbb4189a0b10bc1de55c697d476dd5b
Julian Kurzendoerfer <kurzendoerfer.j@gmail.com>
2025-10-27 12:42:41 +0100

    frontend: make TrendPanel.ohlc optional and pass prop from boot

--- frontend/src/components/TrendPanel.tsx
+++ frontend/src/components/TrendPanel.tsx
@@ -3,18 +3,13 @@ import PlotFactory from "react-plotly.js/factory";
 import Plotly from "plotly.js-dist-min";
 const Plot = PlotFactory(Plotly as any);
 
-export type TrendInput = { time:any[]; close:number[]; high:number[]; low:number[] };
+export type TrendInput = { time:any[]; close:number[]; high?:number[]; low?:number[] };
 
-export default function TrendPanel({ ohlc }: { ohlc: TrendInput }) {
-  const time = ohlc.time, close = ohlc.close;
+export default function TrendPanel({ ohlc }: { ohlc?: TrendInput }) {
+  const t = ohlc?.time ?? [];
+  const c = ohlc?.close ?? [];
   const data:any[] = [
-    { x: time, y: close, type: "scatter", mode: "lines", name: "Close", line: { width: 1.6 } }
+    { x: t, y: c, type: "scatter", mode: "lines", name: "Close", line: { width: 1.6 } }
   ];
-  return (
-    <Plot
-      data={data}
-      layout={{ height: 220, margin: { l: 40, r: 10, t: 20, b: 20 }, showlegend: false }}
-      style={{ width: "100%" }}
-    />
-  );
+  return <Plot data={data} layout={{height:220,margin:{l:40,r:10,t:20,b:20},showlegend:false}} style={{width:"100%"}} />;
 }
